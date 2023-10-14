0f7cfd664e214c5acf5de7b813c20926ee4b1b87
nbibrahim <bibrahim@mun.ca>
2023-10-14 17:01:26 -0230

    new

--- train_test/script_parallelization2.py
+++ train_test/script_parallelization2.py
@@ -1,6 +1,8 @@
+# Implementing memory optimization strategies in the code
 import numpy as np
 from multiprocessing import Pool
 import time
+import gc  # Importing the garbage collector
 
 ##Wavenumbers
 k = 12 * np.pi
@@ -34,15 +36,12 @@ t_span = (0, 1100)
 dt = 0.001
 epsilon = 0.01  #noise strength (0.01 has been well used)
 
-
-#The function below uses the Euler Maruyama method which is used to solve Stochastic Differential Equations (SDEs)
 def euler_maruyama(y0, t_span, dt, epsilon, seed=None):
     np.random.seed(seed)
     t0, tf = t_span
-    t = np.linspace(t0, tf, int((tf - t0) / dt) + 1)
-    s = np.zeros((4, len(t)))
-    U = np.zeros(len(t))
-
+    t = np.linspace(t0, tf, int((tf - t0) / dt) + 1)  # This line was missing
+    s = np.zeros((4, len(t)), dtype=np.float32)  # Using float32 for less memory consumption
+    U = np.zeros(len(t), dtype=np.float32)
     s[:, 0] = y0[:4].ravel()
     U[0] = y0[4]
 
@@ -57,55 +56,60 @@ def euler_maruyama(y0, t_span, dt, epsilon, seed=None):
     y = np.vstack((s, U))
     return t, y
 
+
 def execute_segment(segment):
     rank, y0, t_span, dt, epsilon = segment
     if y0 is None:
-        y0 = np.array([[0, 0, 0, 0, 0.001]]).T  # Substitute with a default initial condition if y0 is None
+        y0 = np.array([[0, 0, 0, 0, 0.001]], dtype=np.float32).T  # Using float32
     t, y = euler_maruyama(y0, t_span, dt, epsilon)
-    cut_index = int(100 / dt)  # Index corresponding to first 100 seconds
+    cut_index = int(100 / dt)
+    del y0  # Deleting the temporary variable
+    gc.collect()  # Manual garbage collection
     return t[cut_index:], y[:, cut_index:]
 
 def main():
     total_time = 1100000
     dt = 0.001
-    num_processes_list = [8]  # Number of CPU threads to use for different runs
+    num_processes_list = [8]
     epsilon = 0.01
     time_taken = {}
+    
+    all_y = np.array([], dtype=np.float32)  # Initialize to an empty array with float32
 
-    all_y = np.array([])
     for num_processes in num_processes_list:
         start_time = time.time()
-
         each_process_time = total_time / num_processes
         pool = Pool(processes=num_processes)
-
         segments = []
         for rank in range(num_processes):
             initial_time = rank * each_process_time
             final_time = initial_time + each_process_time
             t_span = (initial_time, final_time)
-            y0 = np.array([[0, 0, 0, 0, 0.001]]).T if rank == 0 else None
+            y0 = np.array([[0, 0, 0, 0, 0.001]], dtype=np.float32).T if rank == 0 else None  # Using float32
             segments.append((rank, y0, t_span, dt, epsilon))
 
         results = pool.map(execute_segment, segments)
 
-        # Combine results
+        # Combine results using np.concatenate for better memory management
         all_y = np.concatenate([y if i == 0 else y[:, :] for i, (_, y) in enumerate(results)], axis=1)
 
         end_time = time.time()
         elapsed_time = end_time - start_time
         time_taken[num_processes] = elapsed_time
-
         print(f"Time taken with {num_processes} cores: {elapsed_time} seconds")
 
-    # Create a 't' array to be of the same size as the rows of the 'y' array
-    all_t = np.arange(0, all_y.shape[1] * dt, dt)
+    # Creating the 't' array with float32 for less memory consumption
+    all_t = np.arange(0, all_y.shape[1] * dt, dt, dtype=np.float32)
+
+    del results  # Deleting the temporary variable
+    gc.collect()  # Manual garbage collection
 
     return time_taken, all_t, all_y
 
 # Running the main function and storing the time taken for each run
 if __name__ == "__main__":
-    time_taken, t, y= main()
+    time_taken, t, y = main()
     print(t.shape, y.shape)
     np.save("data/large/t.npy", t)
     np.save("data/large/y.npy", y)
+
